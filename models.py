910f7adfaae81b14bbb02c171e6d8de2d6421fd6
Dennis Minn <minndennis@gmail.com>
2022-12-11 13:14:01 -0500

    small changes

--- models.py
+++ models.py
@@ -4,24 +4,24 @@ import torch.nn.functional as F
 
 
 class Policy(nn.Module):
-    def __init__(self, num_actions):
+    def __init__(self, state_dim, action_dim):
         super().__init__()
-        self.fc1 = nn.Linear(2, 10)
+        self.fc1 = nn.Linear(state_dim, 10)
         self.fc2 = nn.Linear(10, 10)
-        self.fc3 = nn.Linear(10, num_actions)
+        self.fc3 = nn.Linear(10, action_dim)
 
     def forward(self, state):
         state = torch.tensor(state)
         out = F.relu(self.fc1(state))
         out = F.relu(self.fc2(out))
-        out = F.softmax(self.fc3(out), dim=0)
+        out = torch.softmax(self.fc3(out), dim=0)
         return out
 
 
 class StateValue(nn.Module):
-    def __init__(self):
+    def __init__(self, state_dim):
         super().__init__()
-        self.fc1 = nn.Linear(2, 10)
+        self.fc1 = nn.Linear(state_dim, 10)
         self.fc2 = nn.Linear(10, 10)
         self.fc3 = nn.Linear(10, 1)
 
