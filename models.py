585dbb7fce26a9247397ee0d45e5e99ad564a58b
Dennis Minn <minndennis@gmail.com>
2022-12-11 18:26:43 -0500

    working implementation of reinforce

--- models.py
+++ models.py
@@ -6,28 +6,24 @@ import torch.nn.functional as F
 class Policy(nn.Module):
     def __init__(self, state_dim, action_dim):
         super().__init__()
-        self.fc1 = nn.Linear(state_dim, 10)
-        self.fc2 = nn.Linear(10, 10)
-        self.fc3 = nn.Linear(10, action_dim)
+        self.fc1 = nn.Linear(state_dim, 128)
+        self.fc2 = nn.Linear(128, action_dim)
 
     def forward(self, state):
         state = torch.tensor(state)
         out = F.relu(self.fc1(state))
-        out = F.relu(self.fc2(out))
-        out = torch.softmax(self.fc3(out), dim=0)
+        out = torch.softmax(self.fc2(out), dim=0)
         return out
 
 
 class StateValue(nn.Module):
     def __init__(self, state_dim):
         super().__init__()
-        self.fc1 = nn.Linear(state_dim, 10)
-        self.fc2 = nn.Linear(10, 10)
-        self.fc3 = nn.Linear(10, 1)
+        self.fc1 = nn.Linear(state_dim, 128)
+        self.fc2 = nn.Linear(128, 1)
 
     def forward(self, state):
         state = torch.tensor(state)
         out = F.relu(self.fc1(state))
-        out = F.relu(self.fc2(out))
-        out = self.fc3(out)
+        out = self.fc2(out)
         return out
