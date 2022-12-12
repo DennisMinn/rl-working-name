34ec66bf9f91e5d8ccada064d43ac739130cbd12
Dennis Minn <minndennis@gmail.com>
2022-12-12 08:41:58 -0500

    adapted code

--- models.py
+++ models.py
@@ -1,29 +1,48 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.distributions import Categorical
 
 
-class Policy(nn.Module):
+class Actor(nn.Module):
     def __init__(self, state_dim, action_dim):
         super().__init__()
         self.fc1 = nn.Linear(state_dim, 128)
         self.fc2 = nn.Linear(128, action_dim)
 
     def forward(self, state):
-        state = torch.tensor(state)
         out = F.relu(self.fc1(state))
-        out = torch.softmax(self.fc2(out), dim=0)
+        out = F.softmax(self.fc2(out), dim=-1)
         return out
 
+    def sample_action(self, state):
+        state = torch.tensor(state, dtype=torch.float).unsqueeze(0)
 
-class StateValue(nn.Module):
+        m = Categorical(self(state))
+        action = m.sample()
+        log_prob = m.log_prob(action)
+
+        return action.item(), log_prob
+
+    @staticmethod
+    # Doesn't work with auto-grad
+    def td_error(G, state_vals):
+        return F.mse_loss(G, state_vals)
+
+
+class Critic(nn.Module):
     def __init__(self, state_dim):
         super().__init__()
         self.fc1 = nn.Linear(state_dim, 128)
         self.fc2 = nn.Linear(128, 1)
 
     def forward(self, state):
-        state = torch.tensor(state)
         out = F.relu(self.fc1(state))
         out = self.fc2(out)
         return out
+
+    @staticmethod
+    def reinforce_baseline_loss(G, svalues, log_probs):
+        deltas = torch.tensor([g - svalue for g, svalue in zip(G, svalues)])
+        loss = sum([d * -log_p for d, log_p in zip(deltas, log_probs)])
+        return loss
---
+++ utils.py
@@ -0,0 +1,57 @@
+from itertools import repeat, chain
+import functools
+import torch
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def process_rewards(rewards, gamma=1):
+    G = []
+    cum_sum = 0
+    for r in reversed(rewards):
+        cum_sum = r + cum_sum * gamma
+        G.insert(0, cum_sum)
+
+    G = torch.tensor(G, dtype=torch.float)
+    G = (G - G.mean())/G.std()
+    return G
+
+
+def G_t(trajectory, gamma=1):
+    rewards = [reward for _, _, reward in trajectory]
+    _return = functools.reduce(
+        lambda _return, reward: _return + reward[1] * pow(gamma, reward[0]),
+        enumerate(rewards),
+        0
+    )
+
+    return _return
+
+
+def plot_t_per_episode(metrics):
+    t_mean = np.array(metrics["t"]).mean(axis=0)
+    t_std = np.array(metrics["t"]).std(axis=0)
+    episodes = np.arange(len(t_mean))
+
+    plt.yticks(ticks=(np.arange(10)*100))
+    plt.errorbar(episodes, t_mean, t_std, linestyle=None, marker=None, ecolor="#bfe6ff")
+
+
+def plot_action_per_episode(metrics):
+    t_mean = np.array(metrics["t"]).mean(axis=0, dtype=int)
+
+    t_mean = list(
+        chain.from_iterable(
+            repeat(episode, t)
+            for episode, t in enumerate(t_mean)
+        )
+    )
+    plt.plot(t_mean)
+
+
+def plot_return_per_episode(metrics):
+    t_mean = np.array(metrics["returns"]).mean(axis=0)
+    t_std = np.array(metrics["returns"]).std(axis=0)
+    episodes = np.arange(len(t_mean))
+
+    plt.errorbar(episodes, t_mean, t_std, linestyle=None, marker=None, ecolor="#bfe6ff")
