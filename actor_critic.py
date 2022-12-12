2d2f5d4ede7cbb2ef73ccb1edfa3ef984dc4cf60
Dennis Minn <minndennis@gmail.com>
2022-12-12 15:28:00 -0500

    working implementation of actor_critic with refactor code

--- actor_critic.py
+++ actor_critic.py
@@ -1,158 +1,99 @@
-import gym, os
-from itertools import count
+from tqdm.auto import tqdm
+import numpy as np
+import gym
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-from torch.distributions import Categorical
+
 import matplotlib.pyplot as plt
-import numpy as np
-import pdb 
-
-class Actor(nn.Module):
-    def __init__(self, state_size, action_size):
-        super(Actor, self).__init__()
-        self.state_size = state_size
-        self.action_size = action_size
-        self.linear1 = nn.Linear(self.state_size, 128)
-        self.linear3 = nn.Linear(128, self.action_size)
-
-    def forward(self, state):
-        output = F.relu((self.linear1(state)))
-        output = self.linear3(output)
-        distribution = F.softmax(output, dim=-1)
-        return distribution
+from models import Actor, Critic
 
 
-class Critic(nn.Module):
-    def __init__(self, state_size, action_size):
-        super(Critic, self).__init__()
-        self.state_size = state_size
-        self.action_size = action_size
-        self.linear1 = nn.Linear(self.state_size, 128)
-        self.linear3 = nn.Linear(128, 1)
+def actor_critic_cartpole():
+    gamma = 0.9
+    env = gym.make('CartPole-v1')
+    state_size = env.observation_space.shape[0]
+    action_size = env.action_space.n
 
-    def forward(self, state):
-        output = F.relu((self.linear1(state)))
-        # output = F.relu(self.linear2(output))
-        value = self.linear3(output)
-        return value
+    count_ca = []
+    count_tca = []
 
-def select_action(network, state):
-    ''' Selects an action given current state
-    Args:
-    - network (Torch NN): network to process state
-    - state (Array): Array of action space in an environment
+    count_episode = range(1, 2000+1)
 
-    Return:
-    - (int): action that is selected
-    - (float): log probability of selecting that action given state and network
-    '''
+    for k in range(1):
+        env = gym.make('CartPole-v1')
+        actor = Actor(state_size, action_size)
+        critic = Critic(state_size)
 
-    #convert state to float tensor, add 1 dimension, allocate tensor on device
-    # state = torch.from_numpy(state).float().unsqueeze(0)
+        count_actions = []
+        total_count_actions = []
+        total_a = 0
 
-    #use network to predict action probabilities
-    # print(network(state))
-    # pdb.set_trace()
-    action_probs = network(state.unsqueeze(0))
-    state = state.detach()
+        for episode in tqdm(range(1, 2000+1)):
+            actor_optim = optim.SGD(actor.parameters(), lr=0.001)
+            critic_optim = optim.SGD(critic.parameters(), lr=0.001)
+            state = env.reset()
+            isTerminal = False
+            score = 0
+            count_a = 0
 
-    #sample an action using the probability distribution
-    m = Categorical(action_probs)
-    action = m.sample()
+            while not isTerminal:
+                count_a += 1
 
-    #return action
-    return action, m.log_prob(action)
+                action, log_prob = actor.sample_action(state)
+                state_tensor = torch.from_numpy(state).float().unsqueeze(0)
+                v_curr = critic(state_tensor)
+                state_prime, reward, isTerminal, info = env.step(action)
+                state_prime_tensor = torch.from_numpy(state_prime).float().unsqueeze(0)
+                v_next = critic(state_prime_tensor)
 
+                score += reward
 
+                if isTerminal:
+                    v_next = torch.tensor([0]).float().unsqueeze(0)
 
-gamma = .1
-num_episodes = 100
-num_steps = 10000
-env = gym.make("MountainCar-v0")
-# env = gym.make('CartPole-v1')
-state_size = env.observation_space.shape[0]
-action_size = env.action_space.n
+                critic_loss = F.mse_loss(reward + gamma * v_next, v_curr)
+                advantage = reward + gamma*v_next.item()-v_curr.item()
+                actor_loss = -log_prob * advantage
 
-actor = Actor(state_size, action_size)
-critic = Critic(state_size, action_size)
+                actor_optim.zero_grad()
+                actor_loss.backward(retain_graph=True)
+                actor_optim.step()
 
-count_episode = range(1,num_episodes+1)
-count_actions = []
-total_count_actions = []
-total_a = 0
-for episode in range(num_episodes):
-    actor_optim = optim.Adam(actor.parameters(),lr=0.05)
-    critic_optim = optim.Adam(critic.parameters(), lr=0.05)
-    state = env.reset()
-    isTerminal = False
-    score = 0
-    
-    count_a = 0
-    
-    for i in range(num_steps):       
-        count_a += 1
-        state = torch.FloatTensor(state)
-        with torch.no_grad():
-            action, log_prob = select_action(network=actor, state=state)
-            # action = torch.argmax(actor(state))
-            # log_prob = torch.max(actor(state))
-        state_prime, reward, isTerminal, info = env.step(action.item())
-        state_prime = torch.FloatTensor(state_prime)
-        if state_prime[0] >= 0.5:
-            print(f'Num episodes {episode}, num actions {i} {isTerminal}')
-            v_next = torch.tensor([0]).float().unsqueeze(0)
-        # if isTerminal:
-        #     print(f'Num episodes {episode}, num actions {i} {isTerminal}')
-        v_curr = critic(state)
-        v_next = critic(state_prime)
-            
+                critic_optim.zero_grad()
+                critic_loss.backward()
+                critic_optim.step()
 
-        td_target = reward + gamma * v_next
-        td_error = reward + ((gamma*v_next)-v_curr)
-        
-        # print(v_curr)
-        # print(log_prob)
-        # Policy
-        actor_loss = (td_error)
-        actor_loss *= -log_prob
-        actor_optim.zero_grad()
-        actor_loss.backward(retain_graph=True)
-        actor_optim.step()
+                if isTerminal:
+                    break
 
-        # Value
-        critic_loss = F.mse_loss(td_target,v_curr)
-        critic_optim.zero_grad()
-        critic_loss.backward()
-        critic_optim.step()
-        state = state_prime
+                state = state_prime
 
-        # print(f'Actor loss is {actor_loss} and critic loss is {critic_loss}')
-        if state_prime[0] >= 0.5:
-            break
-        # print(state)
-    print(count_a)
-    count_actions.append(count_a)
-    total_a += count_a
-    total_count_actions.append(total_a)
-      
-torch.save(actor, 'actor.pkl')
-torch.save(critic, 'critic.pkl')
-env.close()        
+        count_actions.append(count_a)
+        total_a += count_a
+        total_count_actions.append(total_a)
 
-plt.figure()
-plt.title('Count of Episodes vs Count of Actions')
-plt.xlabel('Count of Episodes')
-plt.ylabel('Count of Actions')
-plt.plot(count_episode, count_actions)
-plt.savefig('count_actions_ac.jpg')
-plt.show()
+    env.close()
+    count_ca.append(count_actions)
+    count_tca.append(total_count_actions)
 
-plt.figure()
-plt.title('Total Actions vs Count of Episodes ')
-plt.ylabel('Count of Episodes')
-plt.xlabel('Total Count of Actions')
-plt.plot(total_count_actions, count_episode)
-plt.savefig('total_actions_ac.jpg')
-plt.show()
+    avg_ca = np.array(count_ca)
+    avg_ca = np.average(count_ca, axis=0)
+    plt.figure()
+    plt.title('Count of Episodes vs Count of Actions')
+    plt.xlabel('Count of Episodes')
+    plt.ylabel('Count of Actions')
+    plt.plot(count_episode, avg_ca)
+    plt.savefig('count_actions_ac.jpg')
+    plt.show()
+
+    avg_tca = np.array(count_tca)
+    avg_tca = np.average(count_tca, axis=0)
+    plt.figure()
+    plt.title('Total Actions vs Count of Episodes ')
+    plt.ylabel('Count of Episodes')
+    plt.xlabel('Total Count of Actions')
+    plt.plot(avg_tca, count_episode)
+    plt.savefig('total_actions_ac.jpg')
+    plt.show()
